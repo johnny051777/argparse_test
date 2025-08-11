0fb26db118a353bce7c4be3a288b3e9827999bfe
johnny051777 <153641571+johnny051777@users.noreply.github.com>
2025-08-11 20:33:52 +0800

    Add files via upload

---
+++ arg1.py
@@ -0,0 +1,24 @@
+import argparse
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("base",type=int,help="第一個數字")
+parser.add_argument("-add",type=int,nargs="+",help="負責加法",dest="A")
+parser.add_argument("-sub",type=int,help="負責減法")
+parser.add_argument("-mul",type=int,help="負責乘法")
+parser.add_argument("-div",type=int,help="負責除法")
+
+args = parser.parse_args()
+
+result = args.base
+
+if args.A:
+    result += sum(args.A)
+if args.sub:
+    result -= args.sub
+if args.mul:
+    result *= args.mul
+if args.div:
+    result /= args.div
+
+print(result)
