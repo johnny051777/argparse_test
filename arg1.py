e96b89b4ad2eb1f38c1a7056042c8a8521872458
johnny051777 <153641571+johnny051777@users.noreply.github.com>
2025-08-12 23:46:21 +0800

    Update arg1.py

--- arg1.py
+++ arg1.py
@@ -3,7 +3,7 @@ import argparse
 parser = argparse.ArgumentParser()
 
 parser.add_argument("base",type=int,help="第一個數字")
-parser.add_argument("-add",type=int,nargs="+",help="負責加法",dest="A")  ->nargs為+時，自動回傳一個list
+parser.add_argument("-add",type=int,nargs="+",help="負責加法",dest="A")  # ->nargs為+時，自動回傳一個list
 parser.add_argument("-sub",type=int,help="負責減法")
 parser.add_argument("-mul",type=int,help="負責乘法")
 parser.add_argument("-div",type=int,help="負責除法")
@@ -23,3 +23,4 @@ if args.div:
 
 
 print(result)
+
